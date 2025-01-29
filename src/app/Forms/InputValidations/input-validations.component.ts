98be21ae0acdcc53b8895ec06aa1c81a800f02d7
UZENDMA <matthew.zendejas@savvas.com>
2025-01-29 17:19:22 -0500

    added styles to input-field-validation

--- src/app/Forms/InputValidations/input-validations.component.ts
+++ src/app/Forms/InputValidations/input-validations.component.ts
@@ -1,21 +1,38 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, HostListener, AfterViewInit } from '@angular/core';
+import { ReflowService } from 'src/app/Services/reflow.service';
 
 @Component({
   selector: 'InputValidations',
   templateUrl: './input-validations.component.html',
   styleUrls: ['./input-validations.component.scss'],
 })
-export class InputValidationsComponent implements OnInit {
+export class InputValidationsComponent implements AfterViewInit {
   @ViewChild('inputValidation1') inputElement?: ElementRef; // Access the element
   @ViewChild('inputValidation2') inputElement2?: ElementRef; // Access the element
   errorMessages: { [key: string]: string } = {};
-  constructor() {}
 
-  ngOnInit(): void {
+  constructor(public reflowService: ReflowService) {}
+
+  ngAfterViewInit(): void {
+    this.reflowService.checkViewport();
+
     this.errorMessages = {
       inputValidation1: '',
       inputValidation2: '',
     };
+  }
+
+    // Listens to the window resize event
+  // The '$event' is not necessary for this case but can be useful for debugging
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.reflowService.checkViewport();
+  }
+  
+  // Getter to expose the isMobile property from the ReflowService to the template
+  // This allows us to conditionally render elements in the template based on whether the screen is mobile-sized
+  get isMobile() {
+    return this.reflowService.isMobile;
   }
 
   onBlur(inputId: string): void {
