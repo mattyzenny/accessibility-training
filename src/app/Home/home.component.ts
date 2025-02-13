d2b87bf573ff5491c1950b5c2f454eeafc75a9fa
UZENDMA <matthew.zendejas@savvas.com>
2025-02-12 22:45:26 -0500

    added Support Card

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'Accessibility Training';
+  emailPath: string = 'https://mail.google.com/mail/?view=cm&to=matthew.zendejas@savvas.com&su=Feedback%20or%20Issue%20Report';
 
   constructor() { }
 
