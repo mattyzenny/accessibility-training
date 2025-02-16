04558b8dfe7aab249222f7d53fcff5fbb99da835
UZENDMA <matthew.zendejas@savvas.com>
2025-02-16 09:20:25 -0500

    change latest start end lines

--- src/app/Resources/resources.component.ts
+++ src/app/Resources/resources.component.ts
@@ -21,6 +21,32 @@ export class ResourcesComponent implements OnInit {
   }
   
   ngOnInit(): void {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     this.definitions = [
       {
         term: 'Assistive Technology',
@@ -28,8 +54,8 @@ export class ResourcesComponent implements OnInit {
           'Assistive technology is simply a piece of equipment or software that helps someone with a disability to do something that they would otherwise find difficult or impossible. Screen readers, magnifiers, and alternative keyboards are examples of assistive technology.',
         link: 'https://www.deque.com/blog/assistive-technology/#:~:text=Assistive%20technology%20is%20simply%20a,to%20name%20just%20a%20few.',
         updated: 'Loading...',
-        startLine: 17,
-        endLine: 25
+        startLine: 51,
+        endLine: 59
       },
       {
         term: 'WebAIM',
@@ -37,28 +63,26 @@ export class ResourcesComponent implements OnInit {
           'WebAIM (Web Accessibility In Mind) is a non-profit organization based at the Center for Persons with Disabilities at Utah State University. WebAIM has provided comprehensive web accessibility solutions since 1999. These years of experience have made WebAIM one of the leading providers of web accessibility expertise internationally.',
         link: 'https://webaim.org/',
         updated: 'Loading...',
-        startLine: 27,
-        endLine: 33,
+        startLine: 60,
+        endLine: 68,
       },
-
       {
         term: 'Deque Systems',
         definition:
           'Deque Systems is a company that specializes in digital accessibility. Deque provides software, training, and consulting services to help organizations meet their accessibility goals. Deque has been a leader in the field of digital accessibility since 1999.',
         link: 'https://www.deque.com/',
         updated: 'Loading...',
-        startLine: 35,
-        endLine: 43
+        startLine: 69,
+        endLine: 77
       },
-
       {
         term: 'W3CC',
         definition:
           "The World Wide Web Consortium (W3C) is an international community that develops open standards to ensure the long-term growth of the Web. W3C is made up of member organizations, full-time staff, and the public. W3C's mission is to lead the Web to its full potential by developing protocols and guidelines that ensure the long-term growth of the Web.",
         link: 'https://www.w3.org/',
         updated: 'Loading...',
-        startLine: 46,
-        endLine: 53
+        startLine: 78,
+        endLine: 86
       },
     ];
       // Call API using async/await .subscribe() and Loop through each item and fetch its last updated time 
