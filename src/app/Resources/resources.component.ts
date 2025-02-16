e9130f965a9377d75c7554c57f1c533c890f21d9
UZENDMA <matthew.zendejas@savvas.com>
2025-02-16 14:20:52 -0500

    adding

--- src/app/Resources/resources.component.ts
+++ src/app/Resources/resources.component.ts
@@ -57,8 +57,7 @@ export class ResourcesComponent implements OnInit {
       },
       {
         term: 'Assistive Technology',
-        definition:
-          'Assistive technology is simply a piece of equipment or software that helps someone with a disability to do something that they would otherwise find difficult or impossible. Screen readers, magnifiers, and alternative keyboards are examples of assistive technology.',
+        definition: 'Assistive technology is simply a piece of equipment or software that helps someone with a disability to do something that they would otherwise find difficult or impossible. Screen readers, magnifiers, and alternative keyboards are examples of assistive technology.',
         link: 'https://www.deque.com/blog/assistive-technology/#:~:text=Assistive%20technology%20is%20simply%20a,to%20name%20just%20a%20few.',
         updated: 'Loading...',
         startLine: 51,
@@ -66,8 +65,7 @@ export class ResourcesComponent implements OnInit {
       },
       {
         term: 'WebAIM',
-        definition:
-          'WebAIM (Web Accessibility In Mind) is a non-profit organization based at the Center for Persons with Disabilities at Utah State University. WebAIM has provided comprehensive web accessibility solutions since 1999. These years of experience have made WebAIM one of the leading providers of web accessibility expertise internationally.',
+        definition: 'WebAIM (Web Accessibility In Mind) is a non-profit organization based at the Center for Persons with Disabilities at Utah State University. WebAIM has provided comprehensive web accessibility solutions since 1999. These years of experience have made WebAIM one of the leading providers of web accessibility expertise internationally.',
         link: 'https://webaim.org/',
         updated: 'Loading...',
         startLine: 60,
@@ -75,13 +73,7 @@ export class ResourcesComponent implements OnInit {
       },
       {
         term: 'Deque Systems',
-        link: 'https://www.deque.com/',
-        startLine: 69,
-      },
-      {
-        term: 'Defjsdlkajfsklque Systems',
-        definition:
-          'Deque Systems is a company that specializes in digital accessibility. Deque provides software, training, and consulting services to help organizations meet their accessibility goals. Deque has been a leader in the field of digital accessibility since 1999.',
+        definition: 'Deque Systems is a company that specializes in digital accessibility. Deque provides software, training, and consulting services to help organizations meet their accessibility goals. Deque has been a leader in the field of digital accessibility since 1999.',
         link: 'https://www.deque.com/',
         updated: 'Loading...',
         startLine: 69,
@@ -89,8 +81,7 @@ export class ResourcesComponent implements OnInit {
       },
       {
         term: 'W3C',
-        definition:
-          "The World Wide Web Consortium (W3C) is an international community that develops open standards to ensure the long-term growth of the Web. W3C is made up of member organizations, full-time staff, and the public. W3C's mission is to lead the Web to its full potential by developing protocols and guidelines that ensure the long-term growth of the Web.",
+        definition: "The World Wide Web Consortium (W3C) is an international community that develops open standards to ensure the long-term growth of the Web. W3C is made up of member organizations, full-time staff, and the public. W3C's mission is to lead the Web to its full potential by developing protocols and guidelines that ensure the long-term growth of the Web.",
         link: 'https://www.w3.org/',
         updated: 'Loading...',
         startLine: 78,
