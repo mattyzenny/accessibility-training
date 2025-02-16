ca5f2778e15c5585f5c467e503e83456b83f2bba
UZENDMA <matthew.zendejas@savvas.com>
2025-02-16 14:49:55 -0500

    Testing

--- src/app/Resources/resources.component.ts
+++ src/app/Resources/resources.component.ts
@@ -81,7 +81,7 @@ export class ResourcesComponent implements OnInit {
       },
       {
         term: 'W3C',
-        definition: " World Wide Web Consortium (W3C) is an international community that develops open standards to ensure the long-term growth of the Web. W3C is made up of member organizations, full-time staff, and the public. W3C's mission is to lead the Web to its full potential by developing protocols and guidelines that ensure the long-term growth of the Web.",
+        definition: "The World Wide Web Consortium (W3C) is an international community that develops open standards to ensure the long-term growth of the Web. W3C is made up of member organizations, full-time staff, and the public. W3C's mission is to lead the Web to its full potential by developing protocols and guidelines that ensure the long-term growth of the Web.",
         link: 'https://www.w3.org/',
         updated: 'Loading...',
         startLine: 78,
