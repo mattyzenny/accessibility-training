173a4724706b5093c735664571a693c580d8a19e
UZENDMA <matthew.zendejas@savvas.com>
2025-02-17 11:29:43 -0500

    Test updates.json automation

--- src/app/Services/git.service.ts
+++ src/app/Services/git.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,7 +16,12 @@ export class GitService {
       .set('filePath', filePath)
       .set('startLine', startLine.toString())
       .set('endLine', endLine.toString());
-
-    return this.http.get<{ updated: string }>(`${this.apiUrl}`, { params });
+  
+    return this.http.get<{ definition: { updated: string } }>(`${this.apiUrl}`, { params }).pipe(
+  map(response => {
+    // Check if updated is nested, and return it as a string
+    return { updated: response.definition.updated || 'Unknown' }; // Use 'Unknown' if updated is missing
+  })
+);
   }
-}
+};
