98bdb81e333bdcc9f2f7abac4fb43e942238fd24
UZENDMA <matthew.zendejas@savvas.com>
2025-03-02 12:35:52 -0500

    adding content to checkbox group

--- src/app/Pages/GroupedInputs/Checkbox/checkbox.component.ts
+++ src/app/Pages/GroupedInputs/Checkbox/checkbox.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ReflowService } from 'src/app/Services/ReflowService/reflow.service';
 
 @Component({
   selector: 'Checkbox',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent implements OnInit {
-
-  constructor() { }
+  checkboxList: any[] = [];
+  title = 'Checkbox';
+  constructor(public reflowService: ReflowService) {}
 
   ngOnInit(): void {
+    this.checkboxList = [
+      {
+        label: 'Color',
+        checkboxGroup: [
+          { label: 'Blue' },
+          { label: 'Red' },
+          { label: 'Green' },
+        ],
+      },
+      {
+        label: 'Color',
+        checkboxGroup: [
+          { label: 'Blue' },
+          { label: 'Red' },
+          { label: 'Green' },
+        ],
+      }
+    ];
   }
 
+  ngAfterViewInit() {
+    this.reflowService.checkViewport();
+  }
+  // Listens to the window resize event
+  // The '$event' is not necessary for this case but can be useful for debugging
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.reflowService.checkViewport();
+  }
+
+  // Getter to expose the isMobile property from the ReflowService to the template
+  // This allows us to conditionally render elements in the template based on whether the screen is mobile-sized
+  get isMobile() {
+    return this.reflowService.isMobile;
+  }
 }
