4d76bce013cbc95b58ec91c9d12654ff5455adb3
UZENDMA <matthew.zendejas@savvas.com>
2025-03-16 19:03:36 -0400

    update high contrast mode

--- src/app/Pages/AdvancedDesigns/HighContrast/high-contrast.component.ts
+++ src/app/Pages/AdvancedDesigns/HighContrast/high-contrast.component.ts
@@ -3,21 +3,19 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'HighContrast',
   templateUrl: './high-contrast.component.html',
-  styleUrls: ['./high-contrast.component.scss']
+  styleUrls: ['./high-contrast.component.scss'],
 })
 export class HighContrastComponent implements OnInit {
   title = 'High Contrast';
   @ViewChild('toggle') toggle!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  toggleHighContrast (event: Event) {
+  ngOnInit(): void {}
+  toggleHighContrast(event: Event) {
     if (this.toggle.nativeElement.active) {
       document.body.classList.add('high-contrast');
-    }
-    else {
+    } else {
       document.body.classList.remove('high-contrast');
     }
   }
